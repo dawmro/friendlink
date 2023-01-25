2d48a8e894ad6c946d2a269e5b77744e53812fd8
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-25 09:28:12 +0100

    add login_required

--- friendlink/core/views.py
+++ friendlink/core/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from . models import Profile
 
 # Create your views here.
 
 # home path view
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
