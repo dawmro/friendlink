56eeca85baa11134dea12d9dc7ee0c159f198ebb
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-25 01:43:16 +0100

    verify signup data

--- friendlink/core/views.py
+++ friendlink/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
 
 # Create your views here.
@@ -14,6 +16,22 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-    else
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save
+
+        else:
+            messages.info(request, 'Passwords does not match')
+            return redirect('signup')
+
+    else:
         return render(request, 'signup.html')
 
