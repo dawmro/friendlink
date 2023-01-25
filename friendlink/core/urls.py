0cc5e3183c8a33fa4e0ef3d2f368cca81fa938a8
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-25 02:46:26 +0100

    add logout path to urlpatterns

--- friendlink/core/urls.py
+++ friendlink/core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # home path
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin')
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout')
 ]
