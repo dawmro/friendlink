852db590bb5a537561f77ad57c6a3e9008bc69b7
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-25 02:13:20 +0100

    add signin path to urlpatterns

--- friendlink/core/urls.py
+++ friendlink/core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # home path
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup')
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin')
 ]
