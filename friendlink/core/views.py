b928ac311c6b14133288d891b539df98e8e915a6
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-25 02:05:43 +0100

    create Profile object for new user

--- friendlink/core/views.py
+++ friendlink/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from . models import Profile
 
 # Create your views here.
 
@@ -26,7 +27,15 @@ def signup(request):
                 return redirect('signup')
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
-                user.save
+                user.save()
+
+                # log user in and redirect to settings page
+
+                # create Profile object for new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
 
         else:
             messages.info(request, 'Passwords does not match')
