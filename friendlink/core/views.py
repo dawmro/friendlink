2e10c9180490aaef9f0c6f4c79b9d7764ef94940
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-30 07:28:36 +0100

    add save post

--- friendlink/core/views.py
+++ friendlink/core/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from . models import Profile
+from . models import Profile, Post
 
 # Create your views here.
 
@@ -128,6 +128,11 @@ def upload(request):
         user = request.user.username
         image = request.FILES.get('image_upload')
         caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
     else:
         return redirect('/')
     return HttpResponse('<h1> Upload View </h1>')
