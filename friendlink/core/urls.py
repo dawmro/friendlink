23042c5a11980b47edb364f8b4132c5ae39a94c6
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-30 14:47:11 +0100

    add like-post path to urlpatterns

--- friendlink/core/urls.py
+++ friendlink/core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
     path('settings', views.settings, name='settings'),
-    path('upload', views.upload, name='upload')
+    path('upload', views.upload, name='upload'),
+    path('like-post', views.like-post, name='like-post')
 ]
